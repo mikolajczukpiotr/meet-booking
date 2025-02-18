77933c1af59c3162970e50dad51500d6c6f05cef
Piotr Mikolajczuk <piotr@mikolajczuk.in>
2025-02-18 21:51:07 +0100

    feat: update app structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { BookingForm } from './components/BookingForm';
 
 export const App: React.FC = () => {
-  
   return (
-    <div className="min-h-screen bg-gray-50 p-8">
-      <p>Meet Booking</p>
+    <div className="min-h-screen bg-[#F9F5FF]">
+      <BookingForm />
     </div>
   );
 };
