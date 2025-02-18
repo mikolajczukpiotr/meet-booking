defd8535ef755d8f6b623b5b03c6e326c4020645
Piotr Mikolajczuk <piotr@mikolajczuk.in>
2025-02-18 20:14:27 +0100

    init app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <p>Meet Booking</p>
+    </div>
+  );
+};
